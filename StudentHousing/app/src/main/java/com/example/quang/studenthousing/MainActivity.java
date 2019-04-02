7958d54602ae3e73697117c2d35b720e685273f7
Nguyen Anh Toan <toantictic@gmail.com>
2019-04-02 15:33:25 +0700

    - init project

---
+++ StudentHousing/app/src/main/java/com/example/quang/studenthousing/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.quang.studenthousing;
+
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
