cf9042b88f18b05801e406656861e65a9e6775ab
phuc001122 <phuc001122@gmail.com>
2019-04-03 08:24:38 +0700

    sprint1: layout va activity khoi dong

--- StudentHousing/app/src/main/java/com/example/quang/studenthousing/SplashActivity.java
+++ StudentHousing/app/src/main/java/com/example/quang/studenthousing/SplashActivity.java
@@ -23,8 +23,6 @@ public class SplashActivity extends AppCompatActivity {
             finish();
             return;
         }
-
-
          new CountDownTimer(3000,1000)
          {
             @Override
