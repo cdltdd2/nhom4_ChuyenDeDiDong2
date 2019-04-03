5d0f306bc44bf18e3b74c7a4a30a205894985689
phuc001122 <phuc001122@gmail.com>
2019-04-03 08:13:21 +0700

    sprint1: layout va activity khoi dong

---
+++ StudentHousing/app/src/main/java/com/example/quang/studenthousing/SplashActivity.java
@@ -0,0 +1,62 @@
+package com.example.quang.studenthousing;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.quang.studenthousing.utils.Utils;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+        if (!Utils.checkPermission(this) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            startActivity(new Intent(this,PermissionActivity.class));
+            finish();
+            return;
+        }
+
+
+         new CountDownTimer(3000,1000)
+         {
+            @Override
+            public void onTick(long l)
+            {
+
+            }
+
+            @Override
+            public void onFinish()
+            {
+                SharedPreferences pre = getSharedPreferences("studenthousing", MODE_PRIVATE);
+                String user = pre.getString("user","");
+                if (!user.equalsIgnoreCase(""))
+                {
+                    String[] arr = user.split("-");
+
+                    int permission = Integer.parseInt(arr[5]);
+                    if (permission == 0 || permission == 2){
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        finish();
+                    }
+                    else if (permission == 1)
+                    {
+                        startActivity(new Intent(SplashActivity.this, ManagerActivity.class));
+                        finish();
+                    }
+
+                }
+
+
+            }
+        }.start();
+    }
+}
