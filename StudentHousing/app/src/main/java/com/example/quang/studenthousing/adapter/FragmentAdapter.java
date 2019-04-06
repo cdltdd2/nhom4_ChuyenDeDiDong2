1405513850875dff513512ff7065e7dfdeb4247a
Nguyen Anh Toan <toantictic@gmail.com>
2019-04-06 14:16:48 +0700

    Trạng thái tab trong layout quản lý

---
+++ StudentHousing/app/src/main/java/com/example/quang/studenthousing/adapter/FragmentAdapter.java
@@ -0,0 +1,39 @@
+package com.example.quang.studenthousing.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.quang.studenthousing.R;
+import com.example.quang.studenthousing.fragment.ManagerPostFragment;
+import com.example.quang.studenthousing.fragment.ManagerUserFragment;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private static int NUM_ITEMS = 2;
+    private Context context;
+
+    public FragmentAdapter(Context context, FragmentManager fragmentManager) {
+        super(fragmentManager);
+        this.context = context;
+    }
+
+    // Returns total number of pages
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    // Returns the fragment to display for that page
+    @Override
+    public Fragment getItem(int position) {
+		return null;
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return null;
+    }
+
+}
