4c041fb469bc09ba065ba34346d6c6ae330d550d
NguyenLinh02 <45090339+NguyenLinh02@users.noreply.github.com>
2019-04-11 13:36:07 +0700

    update create post
    trạng thái: chưa kết nối db để lấy dữ liệu địa chỉ
    chỉnh sửa hàm initview để hiển thi các trường nhập
    thêm hàm getData để lấy user hiện tại

--- StudentHousing/app/src/main/java/com/example/quang/studenthousing/CreatePostActivity.java
+++ StudentHousing/app/src/main/java/com/example/quang/studenthousing/CreatePostActivity.java
@@ -1,22 +1,43 @@
 package com.example.quang.studenthousing;
 
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import com.example.quang.studenthousing.adapter.GridViewImageAdapter;
+import com.google.android.gms.maps.model.LatLng;
 import java.util.ArrayList;
 
-public class CreatePostActivity extends AppCompatActivity
+
+public class CreatePostActivity extends AppCompatActivity implements View.OnClickListener
 {
 
+    private EditText edtTitle;
+    private Spinner spinnerCity;
+    private Spinner spinnerDistrict;
+    private Spinner spinnerWard;
+    private EditText edtStreet;
+    private RadioGroup groupObject;
+    private RadioButton radObjectMale;
+    private RadioButton radObjectFemale;
+    private RadioButton radObjectBoth;
+    private EditText edtDesc;
+    private EditText edtPhone;
     private SeekBar seekBarAcreage;
     private TextView tvAcreage;
     private SeekBar seekBarPrice;
     private TextView tvPrice;
+    private EditText edtMaxPeo;
     private GridView gvImage;
     private Button btnBrowserImage;
     private Button btnCreate;
@@ -24,6 +45,8 @@ public class CreatePostActivity extends AppCompatActivity
     private ArrayList<String> arrImage;
     private GridViewImageAdapter imageAdapter;
 
+    private int idUser;
+    private boolean checkClickCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +54,165 @@ public class CreatePostActivity extends AppCompatActivity
         setContentView(R.layout.activity_create_post);
 
         findID();
+        getData();
         initViews();
+        loadSpinner();
 
     }
     private void findID() {
-
+        edtTitle = findViewById(R.id.edtTitleAddPost);
+        spinnerCity = findViewById(R.id.spinnerCityAddPost);
+        spinnerDistrict = findViewById(R.id.spinnerDistrictAddPost);
+        spinnerWard = findViewById(R.id.spinnerWardAddPost);
+        edtStreet = findViewById(R.id.edtStreetAddPost);
+        groupObject = findViewById(R.id.groupObjectAddPost);
+        radObjectMale = findViewById(R.id.radMaleAddPost);
+        radObjectFemale = findViewById(R.id.radFemaleAddPost);
+        radObjectBoth = findViewById(R.id.radBothAddPost);
+        edtDesc = findViewById(R.id.edtDescAddPost);
+        edtPhone = findViewById(R.id.edtPhoneAddPost);
         seekBarAcreage = findViewById(R.id.seekBarAcreageAddPost);
         tvAcreage = findViewById(R.id.tvAcreageAddPost);
         seekBarPrice = findViewById(R.id.seekBarPriceAddPost);
         tvPrice = findViewById(R.id.tvPriceAddPost);
+        edtMaxPeo = findViewById(R.id.edtMaxpeoAddPost);
         gvImage = findViewById(R.id.gvImageAddPost);
         btnBrowserImage = findViewById(R.id.btnBrowserAddPost);
         btnCreate = findViewById(R.id.btnCreateAddPost);
         btnBack = findViewById(R.id.btnBackAddPost);
     }
 
+    private void getData(){
+        Intent intent = getIntent();
+        idUser = intent.getIntExtra("idUser",0);
+    }
 
-    private void initViews() {
+    private void initViews()
+    {
+        //hien thi hinh anh
         arrImage = new ArrayList<>();
         imageAdapter = new GridViewImageAdapter(this,R.layout.item_gridview_image,arrImage);
         gvImage.setAdapter(imageAdapter);
+
+        //dien tich
+        seekBarAcreage.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tvAcreage.setText(i + " " + getString(R.string.meter2));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        //gia phong
+        seekBarPrice.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                double r = (double)i/(double)10;
+                tvPrice.setText((double)Math.round(r*10)/10 + " " + getString(R.string.million));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        checkClickCreate = true;
+
+        btnBrowserImage.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        btnCreate.setOnClickListener(this);
     }
 
+
+    //state: chua ket noi db
+    private void loadSpinner(){
+
+        ArrayList<String> arrCityString = new ArrayList<>();
+        ArrayList<String> arrDistrictString = new ArrayList<>();
+        ArrayList<String> arrWardString = new ArrayList<>();
+
+        // spinner city
+        ArrayAdapter<String> adapterCity=new ArrayAdapter<String>(this
+                , android.R.layout.simple_spinner_item, arrCityString);
+        //phải gọi lệnh này để hiển thị danh sách cho Spinner
+        adapterCity.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        //Thiết lập adapter cho Spinner
+        spinnerCity.setAdapter(adapterCity);
+
+
+        // spinner district
+        final ArrayAdapter<String> adapterDistrict = new ArrayAdapter<String>(this
+                , android.R.layout.simple_spinner_item, arrDistrictString);
+        //phải gọi lệnh này để hiển thị danh sách cho Spinner
+        adapterDistrict.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        //Thiết lập adapter cho Spinner
+        spinnerDistrict.setAdapter(adapterDistrict);
+
+
+        // spinner ward
+        final ArrayAdapter<String> adapterWard = new ArrayAdapter<String>(this
+                , android.R.layout.simple_spinner_item, arrWardString);
+        //phải gọi lệnh này để hiển thị danh sách cho Spinner
+        adapterWard.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        //Thiết lập adapter cho Spinner
+        spinnerWard.setAdapter(adapterWard);
+
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            //them hinh anh bai dang
+            case R.id.btnBrowserAddPost:
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 123);
+                break;
+
+            //tao bai
+            case R.id.btnCreateAddPost:
+                //kiem tra trang thai nut tao bai co duoc click ko, neu ko thi tra ve man hinh tao bai, neu co thi thong tin chua dc dien thi chua dang bai dc
+                if (!checkClickCreate) {
+                    return;
+                } else {
+                    checkClickCreate = false;
+                }
+
+                //lay thong tin sau khi nhap
+                String title = edtTitle.getText().toString();
+                String street = edtStreet.getText().toString();
+                String desc = edtDesc.getText().toString();
+                String phone = edtPhone.getText().toString();
+                String city = (String) spinnerCity.getSelectedItem();
+                String distric = (String) spinnerDistrict.getSelectedItem();
+                String ward = (String) spinnerWard.getSelectedItem();
+
+                //kiem tra thong tin, neu dien thieu thong tin
+                if (title.isEmpty() || street.isEmpty() || desc.isEmpty()
+                        || phone.isEmpty() || edtMaxPeo.getText().toString().isEmpty()) {
+
+                    //thong bao cho user biet can dien day du thong tin
+                    Snackbar snackbar = Snackbar
+                            .make(edtDesc, R.string.insert_info, Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    checkClickCreate = true;
+                    return;
+                }
+        }
+    }
 }
