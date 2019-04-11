c8fd4d78473ec089ae1388adc2b829b01f98288d
NguyenLinh02 <45090339+NguyenLinh02@users.noreply.github.com>
2019-04-11 12:32:46 +0700

    update register check state

--- StudentHousing/app/src/main/java/com/example/quang/studenthousing/view/register/RegisterFragment.java
+++ StudentHousing/app/src/main/java/com/example/quang/studenthousing/view/register/RegisterFragment.java
@@ -3,6 +3,7 @@ package com.example.quang.studenthousing.view.register;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +13,9 @@ import android.widget.EditText;
 
 import com.example.quang.studenthousing.AccountActivity;
 import com.example.quang.studenthousing.R;
+import com.example.quang.studenthousing.view.login.LoginFragment;
 
-public class RegisterFragment extends Fragment
-{
+public class RegisterFragment extends Fragment implements View.OnClickListener, ViewRegister {
 
     private EditText edtUser;
     private EditText edtPass;
@@ -31,8 +32,10 @@ public class RegisterFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register,container,false);
         findID(view);
+        initViews();
         return view;
     }
+
     private void findID(View view) {
         edtUser = view.findViewById(R.id.edtUserNameRegister);
         edtPass = view.findViewById(R.id.edtPasswordRegister);
@@ -43,5 +46,75 @@ public class RegisterFragment extends Fragment
         btnRegister = view.findViewById(R.id.btnRegister);
     }
 
+    private void initViews() {
+        activity = (AccountActivity) getActivity();
+        btnBack.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
 
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            //bam nut quay lai sau do hien fragment login
+            case R.id.btnBackRegister:
+                activity.loadFragment(new LoginFragment());
+                break;
+
+            //nhan nut dky sau do dien thong tin
+            case R.id.btnRegister:
+                String user = edtUser.getText().toString();
+                String pass = edtPass.getText().toString();
+                String name = edtName.getText().toString();
+                String phone = edtPhone.getText().toString();
+                String confirmPass = edtConfirmPass.getText().toString();
+
+                //neu thieu 1 trong 5 truong chua dc dien se thong bao yeu cau dien day du thong tin
+                if (user.isEmpty() || pass.isEmpty() || confirmPass.isEmpty() || name.isEmpty()
+                        || phone.isEmpty()){
+                    Snackbar snackbar = Snackbar
+                            .make(edtPass, R.string.insert_info, Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    return;
+                }
+
+                //neu mat khau nhap lai sai so voi truong mat khau se thong bao mat khau nhap lai sai
+                if (!pass.equals(confirmPass)){
+                    Snackbar snackbar = Snackbar
+                            .make(edtPass, R.string.confirm_pass_fail, Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    return;
+                }
+
+               break;
+        }
+    }
+
+    //dky thanh cong
+    @Override
+    public void registerSuccess() {
+        Snackbar snackbar = Snackbar
+                .make(edtPass, R.string.success, Snackbar.LENGTH_LONG);
+        snackbar.show();
+
+        activity.loadFragment(new LoginFragment());
+    }
+
+    //dky that bai
+    @Override
+    public void registerFail() {
+        Snackbar snackbar = Snackbar
+                .make(edtPass, R.string.register_fail, Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
+    //thong bao user nay da co tai khoan
+    @Override
+    public void userExists() {
+        Snackbar snackbar = Snackbar
+                .make(edtPass, R.string.user_exists, Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
 }
