8167697101eed62c74f391d6d78e53570dd96337
datle2402 <dat.le.2402@gmail.com>
2019-04-19 09:45:01 +0700

    Update InfoHouseActivity.java

--- StudentHousing/app/src/main/java/com/example/quang/studenthousing/InfoHouseActivity.java
+++ StudentHousing/app/src/main/java/com/example/quang/studenthousing/InfoHouseActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
